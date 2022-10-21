c5b4699856517f603323c0a26ffd02cd00244f93
Adam <AdamSullivan500@gmail.com>
2022-10-21 18:40:10 +0000

    Change mongoose connection to async

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+import PORT from '../.index.js';
 
 
 const app = express();
@@ -14,7 +15,13 @@ if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
-mongoose.connect(MONGO_URI, { useUnifiedTopology: true });
+try {
+  await mongoose.connect(MONGO_URI, { useUnifiedTopology: true });
+} catch (error) {
+  throw new Error(error);
+}
+
+//mongoose.connect(MONGO_URI, { useUnifiedTopology: true });
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
