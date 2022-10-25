ca1946e3a1f4f78fb5a713037e9efe4d206687c2
Adam <AdamSullivan500@gmail.com>
2022-10-25 16:10:32 +0000

    Close Db connection

--- server/server.js
+++ server/server.js
@@ -9,21 +9,42 @@ const schema = require('./schema/schema');
 const app = express();
 
 
-const MONGO_URI = 'mongodb+srv://new-user:torcida@cluster0.qlcdnfi.mongodb.net/test?retryWrites=true&w=majority';
+const MONGO_URI = "mongodb+srv://new-user:torcida@cluster0.qlcdnfi.mongodb.net/test?retryWrites=true&w=majority";
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
-try {
-  await mongoose.connect(MONGO_URI, { useUnifiedTopology: true });
-} catch (error) {
-  throw new Error(error);
-}
+//Previous error, await only valid as async function and top level bodies of modules
 
-//mongoose.connect(MONGO_URI, { useUnifiedTopology: true });
+mongoose.connect(MONGO_URI, { useUnifiedTopology: true });
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .on('error', error => console.log('Error connecting to MongoLab:', error))
+    
+ 
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      process.exit(0);
+    });
+  });
+
+/*
+
+mongoose.disconnect()
+try {
+	Mongoose.connect(MONGO_URI, MONGO_CONFIG)
+		.then((db) => {
+			console.log('Mongoose connection Established')
+			db.connection.on('error', (err) => { console.error(err) }) // <- print nothing
+			db.connection.on('disconnected', () => { console.log('disconnected') }) // <- print once
+			db.connection.on('reconnected', () => { console.log('reconnected') }) // <- never printed
+		})
+} catch (error) {
+	console.error(error.message)
+	console.log('Mongoose connection Failed')
+	process.exit(1)
+}
+*/
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
