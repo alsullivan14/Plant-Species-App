7a7e352b6765a933e7291136871a6b374ef1bf14
Adam <AdamSullivan500@gmail.com>
2022-11-01 17:08:22 +0000

    Change babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        use: {
-          loader: 'babel-loader',
-          options: {
-            "presets": ["@babel/preset-env", "@babel/preset-react"]
-          },
-        },
+        use: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
       },
